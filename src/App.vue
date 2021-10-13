37ed76097bf0a82961a466e222f2c75ce774b2b0
Jairo <jairogregorio10@gmail.com>
2021-10-13 11:23:15 -0600

    Formulario de Tareas

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <tareas></tareas>
+
+</template>
+
+<script>
+import tareas from './views/tareas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    tareas
+  }
+}
+</script>
+
+
